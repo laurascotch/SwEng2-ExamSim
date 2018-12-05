6923cf68891ea75f92f22b56f5695fa22cab9672
laurascotch <laura.scoccianti@gmail.com>
2018-12-05 12:03:05 +0100

    Aggiunti controlli

--- api.js
+++ api.js
@@ -6,12 +6,18 @@ const string_square = require('./string_square').string_square
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/square', (req, res) => {
-    var mystring = req.query.string; //per prendere il valore del parametro string in un url del tipo .../square?string=mystring
-    //console.log(mystring);
-    var strsquare = string_square(mystring);
-    //console.log(strsquare);
-    var result = {result: strsquare};
-    res.json(result);
+    if(req.query.string || req.query.string==''){
+        var mystring = req.query.string; //per prendere il valore del parametro string in un url del tipo .../square?string=mystring
+        //console.log(mystring);
+        var strsquare = string_square(mystring);
+        //console.log(strsquare);
+        var result = {result: strsquare};
+        res.json(result);
+    }else{
+        strsquare = -1
+        var result = {result: strsquare};
+        res.json(result);
+    }
 })
 
 app.listen(PORT, () => console.log('Example app listening on port' + PORT))
--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
-const url = "https://se2-exam-sim-dec2018.herokuapp.com/square"
+//const url = "https://se2-exam-sim-dec2018.herokuapp.com/square"
+const url = "http://localhost:3000/square"
 
 const getsquare = async url => {
 
@@ -13,4 +14,5 @@ const getsquare = async url => {
 };
 
 var mystring = ''
-getsquare(url+"?string="+mystring);    
+getsquare(url+"?string="+mystring);
+//getsquare(url);    
